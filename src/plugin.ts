7e56827e3d5121ed4828d3eab46c5e2a53b829f0
Belar <belar@belardesign.com>
2024-07-15 11:11:59 +0000

    build: add penpot setup

---
+++ src/plugin.ts
@@ -0,0 +1,4 @@
+penpot.ui.open("Plugin name", "", {
+  width: 500,
+  height: 600,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        plugin: "src/plugin.ts",
+        index: "./index.html",
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
 })

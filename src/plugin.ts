22006e354573a375cc7a48310a0d096c7a40caf1
Belar <belar@belardesign.com>
2024-07-15 12:29:38 +0000

    feat: add themes support

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,21 @@
-penpot.ui.open("Plugin name", "", {
+import { PenpotTheme } from "@penpot/plugin-types";
+
+export type PluginMessageEvent = ThemePluginEvent;
+
+type ThemePluginEvent = {
+  type: "theme";
+  content: PenpotTheme;
+};
+
+penpot.ui.open("Plugin name", `?theme=${penpot.getTheme()}`, {
   width: 500,
   height: 600,
 });
+
+penpot.on("themechange", (theme) => {
+  sendMessage({ type: "theme", content: theme });
+});
+
+function sendMessage(message: PluginMessageEvent) {
+  penpot.ui.sendMessage(message);
+}
